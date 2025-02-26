8da35abbfc4d6d2b35b5a3c6e3b107dc9ab56c45
KeerthiSaiPG <147936219+KeerthiSaiPG@users.noreply.github.com>
2025-02-27 00:56:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -280,5 +280,3 @@ if 'submitted' in st.session_state and st.session_state.submitted:
 
 st.markdown("---")
 st.markdown("🔹 **Disclaimer**: Consult a CA for official tax filing.")
-
-st.markdown("Creatively innovated with passion, by Keerthi Sai.")
